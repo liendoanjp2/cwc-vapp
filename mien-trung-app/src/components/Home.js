7b6b94d0205fc1977b5e64f54252540a4fc1a179
Paul Dang <paul.dang1@veym.net>
2019-10-11 22:48:43 -0600

    Initial React Website

---
+++ mien-trung-app/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './Home.css';
+
+import BasicExample from './BasicRouting';
+import JPNavBar from './NavBar';
+
+function Home() {
+    return (
+        <div className="Home">
+            <JPNavBar />
+            
+        </div>
+    );
+}
+export default Home;
+
+
+
+
